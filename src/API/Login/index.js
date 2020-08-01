8fd9aebedda5f5defa26ffcfed6b3fb8766630eb
Roxane Haddad <roxane.haddad@gmail.com>
2020-08-01 19:10:59 +0200

    .

--- src/API/Login/index.js
+++ src/API/Login/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import {checkIfUserLoggedIn} from "../../redux/actions";
 
 export const login = (email, password, dispatch) => {
-  return axios.post("https://movies-rest-api-web.herokuapp.com/users/login", {email: email, password: password})
+  const headers = {"Access-Control-Allow-Origin": "*"};
+  return axios.post("https://movies-rest-api-web.herokuapp.com/users/login", {email: email, password: password}, {headers: headers})
   .then((res) => {
     sessionStorage.setItem('accessToken', res.data.accessToken);
     sessionStorage.setItem('imageUrl', res.data.imageUrl);
