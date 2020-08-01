0f56a214cce7acf6662ed68c01b086fea654f6ab
Roxane Haddad <roxane.haddad@gmail.com>
2020-08-01 17:50:01 +0200

    trying something about cors

--- src/API/Signup/index.js
+++ src/API/Signup/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const signup = (username, email, password, imageUrl) => {
-  return axios.post("https://movies-rest-api-web.herokuapp.com/users/signup", {username: username, email: email, password: password, imageUrl: imageUrl})
+  const headers = {"Access-Control-Allow-Origin": "*"};
+  return axios.post("https://movies-rest-api-web.herokuapp.com/users/signup", {username: username, email: email, password: password, imageUrl: imageUrl}, {headers: headers})
   .then((res) => {
     console.log("REPONSE SIGNUP POST", res);
     return res;
